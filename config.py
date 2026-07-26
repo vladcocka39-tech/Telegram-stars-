ba9cab61cc89d28a1a5746bb5e0deb169ce098e5
vladcocka39-tech <vladcocka39@gmail.com>
2026-07-26 20:45:52 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+BOT_TOKEN = "8446142646:AAHtluAEL_tDStkMhsGB3zoBo1qeoyFSojU"
+ADMIN_ID = 7991301667
+SBER_NUMBER = "+79966293123"
+SBER_NAME = "Владислав Б."
+
+PRODUCTS = {
+    "stars_50": {"name": "⭐ 50 Telegram Stars", "price": 100},
+    "stars_100": {"name": "⭐ 100 Telegram Stars", "price": 190},
+    "stars_250": {"name": "⭐ 250 Telegram Stars", "price": 450},
+    "stars_500": {"name": "⭐ 500 Telegram Stars", "price": 850},
+    "nft_gift": {"name": "🖼 NFT / Telegram Gift", "price": 350},
+}

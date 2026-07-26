cb300b16b891befaa34b63debcef868214c43df6
vladcocka39-tech <vladcocka39@gmail.com>
2026-07-26 22:30:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
+# ==========================================
+# НАСТРОЙКИ БОТА (ТОЛЬКО ЗВЁЗДЫ TELEGRAM)
+# ==========================================
+
+# 1. Токен бота из @BotFather
 BOT_TOKEN = "8446142646:AAHtluAEL_tDStkMhsGB3zoBo1qeoyFSojU"
+
+# 2. Ваш Telegram ID (сюда бот присылает чеки)
 ADMIN_ID = 7991301667
+
+# 3. Реквизиты Сбербанка для приема оплаты
 SBER_NUMBER = "+79966293123"
 SBER_NAME = "Владислав Б."
 
+# 4. Каталог товаров (Только Telegram Stars)
 PRODUCTS = {
     "stars_50": {"name": "⭐ 50 Telegram Stars", "price": 100},
     "stars_100": {"name": "⭐ 100 Telegram Stars", "price": 190},
     "stars_250": {"name": "⭐ 250 Telegram Stars", "price": 450},
     "stars_500": {"name": "⭐ 500 Telegram Stars", "price": 850},
-    "nft_gift": {"name": "🖼 NFT / Telegram Gift", "price": 350},
+    "stars_1000": {"name": "⭐ 1000 Telegram Stars", "price": 1650},
 }
